6fcece71b8e8a600a7bc2e289603f72c4434cf2c
SoHeeKim <SoHeeKim@192.168.1.107>
2020-05-13 21:02:29 -0700

    Test3

--- PracticeSummer2020/src/demo/RestaurantSelector.java
+++ PracticeSummer2020/src/demo/RestaurantSelector.java
@@ -6,23 +6,28 @@ public class RestaurantSelector {
 		public static void main (String[] args)
 		{
 			
-		String vegetarian;
-		String vegan;
-		String glutenFree;
+		String vegetarian = "";
+		String vegan = "" ;
+		String glutenFree = "" ;
 		
 		
 		Scanner inputDevice = new Scanner(System.in);
 		
-		
+		while (!(vegetarian.equalsIgnoreCase("Yes")&& vegetarian.equalsIgnoreCase("No")))
+		{
 		System.out.println("Is anyone in your party a vegetarian? (Yes/No)");
 		 vegetarian = inputDevice.nextLine();	 	
-		
+		}
+		while (!(vegan.equalsIgnoreCase("Yes")&& vegan.equalsIgnoreCase("No")))
+		{
 		System.out.println("Is anyone in your party a vegan? (Yes/No)");
 		vegan = inputDevice.nextLine();
-		
+		}
+		while (!(glutenFree.equalsIgnoreCase("Yes")&& glutenFree.equalsIgnoreCase("No")))
+		{
 		System.out.println("Is anyone in your party gluten-free? (Yes/No)");
 		glutenFree = inputDevice.nextLine();
-		
+		}
 		
 		System.out.println("Here are the restaurant choice(s):");
 		
